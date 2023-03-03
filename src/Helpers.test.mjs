3a2aa3801f57a9a8c0357ae72dfc7f720616cd30
moeenn <moeen.v8@gmail.com>
2023-03-03 16:32:02 +0500

    added prettier and eslint configs

--- src/Helpers.test.mjs
+++ src/Helpers.test.mjs
@@ -2,7 +2,10 @@ import { test, expect } from "vitest"
 import { Helpers } from "#src/Helpers.mjs"
 
 test("Helpers.double", () => {
+  /** @type {number} */
   const expected = 300
+
+  /** @type {number} */
   const got = Helpers.double(150)
 
   expect(got).toBe(expected)
