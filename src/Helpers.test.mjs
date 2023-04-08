7bca935824662e42e8fe19630ba31e566e43eb14
moeenn <moeen.v8@gmail.com>
2023-04-08 21:57:41 +0500

    structure update

--- src/Helpers.test.mjs
+++ src/Helpers.test.mjs
@@ -2,10 +2,7 @@ import { test, expect } from "vitest"
 import { Helpers } from "#src/Helpers.mjs"
 
 test("Helpers.double", () => {
-  /** @type {number} */
   const expected = 300
-
-  /** @type {number} */
   const got = Helpers.double(150)
 
   expect(got).toBe(expected)
